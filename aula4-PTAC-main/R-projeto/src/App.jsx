e6b23dece6d4965cf0534f0a6ef574d68795e5fc
aluno <lucasgomesnascimento4163@gmail.com>
2026-09-03 13:54:40 -0400

    Atividade

---
+++ aula4-PTAC-main/R-projeto/src/App.jsx
@@ -0,0 +1,11 @@
+import ListaUsuarios from "./components/ListaUsuarios"
+
+export default function App () {
+
+  return (
+    <section>
+        <h1>Lista de ususarios</h1>
+        <ListaUsuarios/>
+    </section>
+  )
+}    
